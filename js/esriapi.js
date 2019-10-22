fc5ff7c19e93db27f5f3ebd3e5187e1f01358123
Matt Silveira <msilveira@tnc.org>
2019-10-22 11:31:16 -0600

    updates area selecetd counter and fixed bug: removing items from the selecetd areas array

--- js/esriapi.js
+++ js/esriapi.js
@@ -31,54 +31,79 @@ function ( 	ArcGISDynamicMapServiceLayer, Extent,Draw, SpatialReference, Query, 
 					q.geometry = point.mapPoint;
 					q.outFields = ["*"];
 					q.returnGeometry = true;
-					qt.execute(q, function (e) {
-						// check to see if you clicked on a feature
-						if(e.features.length > 0){
-							t.areaSelected = '';
-							// check to see which area is selected, then add each selected id to 
-							// that area of the main data object
-							if (t.obj['cda-data-object']['local-option']){
-								if (t.obj['cda-data-object']['local-option-selections'].indexOf(e.features[0].attributes.fid_1) == -1){
-									t.obj['cda-data-object']['local-option-selections'].push(e.features[0].attributes.fid_1)
-									map.graphics.add(new Graphic(e.features[0].geometry, t.selectionSymbol, {id:e.features[0].attributes.fid_1}));
+					// only excute the map click query function if the selected areas is less than 5
+					if (t.obj['cda-data-object']['area-selected-counter'] < 5) {
+						qt.execute(q, function (e) {
+							// check to see if you clicked on a feature
+							if (e.features.length > 0) {
+								t.areaSelected = '';
+								// check to see which area is selected, then add each selected id to 
+								// that area of the main data object
+								if (t.obj['cda-data-object']['local-option']) {
+									if (t.obj['cda-data-object']['local-option-selections'].indexOf(e.features[0].attributes.fid_1) == -1) {
+										t.obj['cda-data-object']['local-option-selections'].push(e.features[0].attributes.fid_1)
+										map.graphics.add(new Graphic(e.features[0].geometry, t.selectionSymbol, {
+											id: e.features[0].attributes.fid_1
+										}));
+									}
+									t.obj['cda-data-object']['area-selected'] = 'local-option';
+
+								} else if (t.obj['cda-data-object']['area-option']) {
+									if (t.obj['cda-data-object']['resource-option']) {
+										if (t.obj['cda-data-object']['resource-option-selections'].indexOf(e.features[0].attributes.RU) == -1) {
+											t.obj['cda-data-object']['resource-option-selections'].push(e.features[0].attributes.RU)
+											map.graphics.add(new Graphic(e.features[0].geometry, t.selectionSymbol, {
+												id: e.features[0].attributes.RU
+											}));
+										}
+										t.obj['cda-data-object']['area-selected'] = 'resource-option';
+
+									} else if (t.obj['cda-data-object']['huc-option']) {
+										if (t.obj['cda-data-object']['huc-option-selections'].indexOf(e.features[0].attributes.gid) == -1) {
+											t.obj['cda-data-object']['huc-option-selections'].push(e.features[0].attributes.gid)
+											map.graphics.add(new Graphic(e.features[0].geometry, t.selectionSymbol, {
+												id: e.features[0].attributes.gid
+											}));
+										}
+										t.obj['cda-data-object']['area-selected'] = 'huc-option';
+
+									} else if (t.obj['cda-data-object']['catchment-option']) {
+										if (t.obj['cda-data-object']['catchment-option-selections'].indexOf(e.features[0].attributes.featureid) == -1) {
+											t.obj['cda-data-object']['catchment-option-selections'].push(e.features[0].attributes.featureid)
+											map.graphics.add(new Graphic(e.features[0].geometry, t.selectionSymbol, {
+												id: e.features[0].attributes.featureid
+											}));
+										}
+										t.obj['cda-data-object']['area-selected'] = 'catchment-option';
+									}
 								}
-								t.areaSelected = 'local-option';
-
-							} else if (t.obj['cda-data-object']['area-option']) {
-								if (t.obj['cda-data-object']['resource-option']) {
-									if (t.obj['cda-data-object']['resource-option-selections'].indexOf(e.features[0].attributes.RU) == -1) {
-										t.obj['cda-data-object']['resource-option-selections'].push(e.features[0].attributes.RU)
-										map.graphics.add(new Graphic(e.features[0].geometry, t.selectionSymbol, {id:e.features[0].attributes.RU}));
-									}
-									t.areaSelected = 'resource-option';
-
-								} else if (t.obj['cda-data-object']['huc-option']) {
-									if (t.obj['cda-data-object']['huc-option-selections'].indexOf(e.features[0].attributes.gid) == -1) {
-										t.obj['cda-data-object']['huc-option-selections'].push(e.features[0].attributes.gid)
-										map.graphics.add(new Graphic(e.features[0].geometry, t.selectionSymbol, {id:e.features[0].attributes.gid}));
-									}
-									t.areaSelected = 'huc-option';
-
-								} else if (t.obj['cda-data-object']['catchment-option']) {
-									if (t.obj['cda-data-object']['catchment-option-selections'].indexOf(e.features[0].attributes.featureid) == -1) {
-										t.obj['cda-data-object']['catchment-option-selections'].push(e.features[0].attributes.featureid)
-										map.graphics.add(new Graphic(e.features[0].geometry, t.selectionSymbol, {id:e.features[0].attributes.featureid}));
-									}
-									t.areaSelected = 'catchment-option';
-								}
+							
+								t.esriapi.handleAreaSelections(t);
 							}
-							t.esriapi.handleAreaSelections(t, t.areaSelected);
-						}
-					})
+						})
+					}else{
+						console.log('you can only select 5 areas')
+					}
 				}
 			},
 			// whenever the map is clicked, IDs of the clicked polygons are saved
 			// in an object, handle the UI portion, display and delete selections
-			handleAreaSelections: function (t, areaSelected) {
+			handleAreaSelections: function (t) {
 				$('.cda-selected-areas').empty();
-				console.log(t.obj['cda-data-object'])
-				$.each(t.obj['cda-data-object'][`${areaSelected}-selections`], (i, v) => {
-					let html = `<div id="${v}" class="cda-selected">ID: ${v}<div class="cda-selected-close"><div>&#10005;</div></div></div>`
+				$.each(t.obj['cda-data-object'][`${t.obj['cda-data-object']['area-selected']}-selections`], (i, v) => {
+					let idText = ''
+					if(t.obj['cda-data-object']['local-option']){
+						idText = 'Field ID: ' 
+					}else{
+						if (t.obj['cda-data-object']['huc-option']) {
+							idText = 'HUC 12: '
+						} else if (t.obj['cda-data-object']['catchment-option']) {
+							idText = 'Catchment ID: '
+						} else if (t.obj['cda-data-object']['resource-option']) {
+							idText = 'Resource Unit ID: '
+						}
+					}
+					let html = `<div id="${v}" class="cda-selected">${idText} <span> ${v}</span><div class="cda-selected-close"><div>&#10005;</div></div></div>`
 					$('.cda-selected-areas').append(html);
 				})
 
@@ -91,14 +116,18 @@ function ( 	ArcGISDynamicMapServiceLayer, Extent,Draw, SpatialReference, Query, 
 								if (id == v.attributes.id) {
 									map.graphics.remove(v); // remove graphic form the map
 									$($(evt.currentTarget).parent()[0]).remove();
-									var filtered = t.obj['cda-data-object'][`${areaSelected}-selections`].filter(function (value, index, arr) {
-										return value != id;
-									});
-									t.obj['cda-data-object'][`${areaSelected}-selections`] = filtered;
+									let index = t.obj['cda-data-object'][`${t.obj['cda-data-object']['area-selected']}-selections`].indexOf(v.attributes.id)
+									if (index > -1) {
+										t.obj['cda-data-object'][`${t.obj['cda-data-object']['area-selected']}-selections`].splice(index, 1);
+									}
+									// update selected area counter when clicking on map or on area close button
+									t.esriapi.handleAreaSelectionsCounter(t);
 								}
 							}
 						}
 					})
+					// update selected area counter when clicking on map or on area close button
+					// t.esriapi.handleAreaSelectionsCounter(t);
 				})
 
 				$(document).on('mouseover mouseleave', '.cda-selected', function (evt) {
@@ -115,10 +144,37 @@ function ( 	ArcGISDynamicMapServiceLayer, Extent,Draw, SpatialReference, Query, 
 						}
 					})
 				})
+				// update selected area counter when clicking on map or on area close button
+				t.esriapi.handleAreaSelectionsCounter(t);
 			}, // end of handle area selections function
+
+			handleAreaSelectionsCounter: function (t) {
+				// console.log('counter')
+				t.obj['cda-data-object']['area-selected-counter']
+				if (t.obj['cda-data-object']['local-option']) {
+					t.obj['cda-data-object']['area-selected-counter'] = t.obj['cda-data-object']['local-option-selections'].length
+				} else {
+					if (t.obj['cda-data-object']['huc-option']) {
+						t.obj['cda-data-object']['area-selected-counter'] = t.obj['cda-data-object']['huc-option-selections'].length
+					} else if (t.obj['cda-data-object']['catchment-option']) {
+						t.obj['cda-data-object']['area-selected-counter'] = t.obj['cda-data-object']['catchment-option-selections'].length
+					} else if (t.obj['cda-data-object']['resource-option']) {
+						t.obj['cda-data-object']['area-selected-counter'] = t.obj['cda-data-object']['resource-option-selections'].length
+					}
+				}
+				$('.cda-number-selected-area span').html(t.obj['cda-data-object']['area-selected-counter'])
+			}, // end of area selections counter function 
+
+
 			// whenever the map is clicked, IDs of the clicked polygons are saved
 			// in an object, handle the UI portion, display and delete selections
 			displayLayers: function (t, value) {
+				console.log('area chnage')
+				// reset area selected counter
+				t.obj['cda-data-object']['area-selected-counter'] = 0
+				// reset area selected HTML	
+				$('.cda-number-selected-area span').html(0)
+
 				t.obj['cda-data-object']['local-option-selections'] = []
 				t.obj['cda-data-object']['resource-option-selections'] = []
 				t.obj['cda-data-object']['huc-option-selections'] = []
